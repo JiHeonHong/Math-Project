9a8e9bdc0df215be6630eaf7a3b1002cbd84c982
DESKTOP-4GHGC44\swpc <wlgjs005@naver.com>
2016-10-31 16:52:59 +0900

    sourceTreeTest.c 파일 추가

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-int main(0
+int main()
 {
   printf("Open Source SW Project\n");
   printf("First GitHub Training");
-  
+
   printf("201244036 LeeSeongmin. \n");
   printf("20124403 JiHeonHong. \n");
   return 0;
